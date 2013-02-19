259cea7c7085fa310c25c9ffa2b7b4f2cca305f3
chaconludovic <ludovic_chacon@hotmail.com>
2013-02-19 20:40:38 +0100

    optimisation saisiepiece

--- src/main/java/com/eldoraludo/ppafadministration/components/SaisiePiece.java
+++ src/main/java/com/eldoraludo/ppafadministration/components/SaisiePiece.java
@@ -4,7 +4,6 @@ import static org.apache.commons.lang.builder.EqualsBuilder.reflectionEquals;
 import static org.apache.commons.lang.builder.HashCodeBuilder.reflectionHashCode;
 import static org.apache.commons.lang.builder.ToStringBuilder.reflectionToString;
 
-import java.util.Date;
 import java.util.List;
 
 import org.apache.tapestry5.EventConstants;
@@ -25,7 +24,6 @@ import com.eldoraludo.ppafadministration.entities.Article;
 import com.eldoraludo.ppafadministration.entities.Client;
 import com.eldoraludo.ppafadministration.entities.Item;
 import com.eldoraludo.ppafadministration.entities.Piece;
-import com.eldoraludo.ppafadministration.entities.TypePiece;
 import com.eldoraludo.ppafadministration.pages.GestionPiece;
 import com.eldoraludo.ppafadministration.support.AjaxLoopHolder;
 
@@ -33,7 +31,7 @@ import com.eldoraludo.ppafadministration.support.AjaxLoopHolder;
 //http://tawus.wordpress.com/2011/07/26/tapestry-ajaxformloop/
 //http://jumpstart.doublenegative.com.au/jumpstart/examples/ajax/formloop1
 //http://tapestry.apache.org/current/apidocs/org/apache/tapestry5/corelib/components/AjaxFormLoop.html
-
+// TODO voir pourquoi on doit ajouter une ligne dans le holder et dans l'objet
 public class SaisiePiece {
 
 	@Parameter(required = false)
@@ -41,9 +39,7 @@ public class SaisiePiece {
 
 	@Property
 	@Persist
-	// @Parameter(required = false)
-	private Piece piece; // TODO voir pourqyoi on porurait pas utiliser
-							// @Parameter pour la modification
+	private Piece piece;
 
 	@Property
 	private FieldValue fieldValue;
@@ -54,26 +50,6 @@ public class SaisiePiece {
 	@InjectPage
 	private GestionPiece gestionPiece;
 
-	@Property
-	@Persist
-	@Validate("required")
-	private Date date;
-
-	@Property
-	@Persist
-	@Validate("required")
-	private String numeroPiece;
-
-	@Property
-	@Persist
-	@Validate("required")
-	private TypePiece type;
-
-	@Property
-	@Persist
-	@Validate("required")
-	private Client client;
-
 	@Inject
 	private SelectModelFactory selectModelFactory;
 
@@ -81,19 +57,11 @@ public class SaisiePiece {
 	@Persist
 	private AjaxLoopHolder<FieldValue> holder;
 
-	// public void onActivate(long id) {
-	//
-	// }
-
 	@SetupRender
 	public void setupRender() {
 		holder = new AjaxLoopHolder<FieldValue>();
 		if (this.id != null) {
 			this.piece = (Piece) session.load(Piece.class, id);
-			this.client = this.piece.getClient();
-			this.date = this.piece.getDate();
-			this.numeroPiece = this.piece.getNumeroPiece();
-			this.type = this.piece.getType();
 			if (this.piece != null) {
 				int i = 0;
 				for (Article article_ : this.piece.getArticles()) {
@@ -105,62 +73,17 @@ public class SaisiePiece {
 			}
 		} else {
 			this.piece = new Piece();
-			this.client = null;
-			this.date = null;
-			this.numeroPiece = null;
-			this.type = null;
 		}
 	}
-
-	// public void onActivate(Piece piece) {
-	//
-	// }
-
-	//
-	// @SetupRender
-	// void setupRender() {
-	// if (this.piece != null) {
-	// this.client = this.piece.getClient();
-	// this.date = this.piece.getDate();
-	// this.numeroPiece = this.piece.getNumeroPiece();
-	// this.type = this.piece.getType();
-	// }
-	// if (holder == null) {
-	// holder = new AjaxLoopHolder<FieldValue>();
-	// if (this.piece != null) {
-	// int i = 0;
-	// for (Article article_ : this.piece.getArticles()) {
-	// FieldValue value = new FieldValue();
-	// value.article = article_;
-	// value.order = i++;
-	// holder.add(value);
-	// }
-	// }
-	// }
-	// }
 
 	@OnEvent(value = EventConstants.SUCCESS, component = "pieceForm")
 	@CommitAfter
 	Object onSuccess() {
-		// if (this.piece == null) {
-		// this.piece = new Piece();
-		// } else {
-		// // this.piece = (Piece) session.merge(this.piece);
-		// }
-		this.piece.setClient(client);
-		this.piece.setDate(date);
-		this.piece.setType(type);
-		this.piece.setNumeroPiece(numeroPiece);
-		// for (FieldValue value : holder.getValues()) {
-		// Article article = value.article;
-		// article.setPiece(this.piece);
-		// // article = (Article) session.merge(article);
-		// this.piece.getArticles().add(article);
-		// }
-		// session.update(piece);
-		session.update(this.piece);
-		//session.persist(this.piece);
-		//this.piece = (Piece) session.load(Piece.class, 1l);
+		if (piece.getId() == null) {
+			session.persist(this.piece);
+		} else {
+			session.update(this.piece);
+		}
 		return gestionPiece;
 	}
 
@@ -195,27 +118,6 @@ public class SaisiePiece {
 		holder.remove(value);
 		session.delete(articleToDelete);
 	}
-
-	// @OnEvent(component = "prixUnitaire", value = UpdateZone.DEFAULT_EVENT)
-	// public void onChangeFromPrixUnitaire(
-	// @RequestParameter(value = "article", allowBlank = true) Article article,
-	// @RequestParameter(value = "prixUnitaire", allowBlank = true) Double
-	// prixUnitaire) {
-	// if (article != null) {
-	// article.setPrixUnitaire(prixUnitaire);
-	// }
-	// ajaxResponseRenderer.addRender(totalZone);
-	// }
-	//
-	// @OnEvent(component = "remise", value = UpdateZone.DEFAULT_EVENT)
-	// public void onChangeFromRemise(
-	// @RequestParameter(value = "article", allowBlank = true) Article article,
-	// @RequestParameter(value = "remise", allowBlank = true) Double remise) {
-	// if (article != null) {
-	// article.setRemise(remise);
-	// }
-	// ajaxResponseRenderer.addRender(totalZone);
-	// }
 
 	public SelectModel getListeItem() {
 		List<Item> items = session.createCriteria(Item.class).list();
