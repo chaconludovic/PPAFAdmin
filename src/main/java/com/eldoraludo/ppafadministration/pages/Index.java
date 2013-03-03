6e226eed0b4ee271ce1d5063536b0b38037f4b72
chaconludovic <ludovic_chacon@hotmail.com>
2013-03-03 20:48:59 +0100

    test d'intégration de highcharts

--- src/main/java/com/eldoraludo/ppafadministration/pages/Index.java
+++ src/main/java/com/eldoraludo/ppafadministration/pages/Index.java
@@ -8,6 +8,9 @@ import org.apache.tapestry5.ioc.annotations.*;
 import org.apache.tapestry5.corelib.components.*;
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.alerts.AlertManager;
+import org.apache.tapestry5.json.JSONObject;
+import org.apache.tapestry5.services.javascript.InitializationPriority;
+import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 import org.hibernate.Session;
 
 import com.eldoraludo.ppafadministration.entities.Piece;
@@ -15,6 +18,7 @@ import com.eldoraludo.ppafadministration.entities.Piece;
 /**
  * Start page of application ppafadministration.
  */
+@Import(library={"context:js/demo.js"})
 public class Index {
 	@Property
 	@Inject
@@ -24,5 +28,22 @@ public class Index {
 	public Date getCurrentTime() {
 		return new Date();
 	}
+    @Inject
+    private JavaScriptSupport javascript;
 
+    public JSONObject getOptions(){
+        JSONObject opt = new JSONObject();
+        opt.put("text", "Source: WorldClimate.com");
+        opt.put("x", -20);
+
+        JSONObject high = new JSONObject();
+        high.put("subtitle", opt);
+
+        return high;
+    }
+
+    @AfterRender
+    public void afterRender(){
+        javascript.addInitializerCall(InitializationPriority.EARLY, "index", new JSONObject());
+    }
 }
