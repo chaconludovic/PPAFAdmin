f733d1fe7579b8483cb1d860fb7b801e6d5a5af6
lchacon <lchacon@financeactive.com>
2013-03-08 14:17:59 +0100

    travail sur le chart

--- src/main/java/com/eldoraludo/ppafadministration/components/MyOwnChart.java
+++ src/main/java/com/eldoraludo/ppafadministration/components/MyOwnChart.java
@@ -1,20 +1,67 @@
 package com.eldoraludo.ppafadministration.components;
 
+import com.eldoraludo.ppafadministration.entities.Piece;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.json.JSONArray;
 import org.apache.tapestry5.json.JSONObject;
-import org.got5.tapestry5.jquery.highcharts.components.AbstractHighCharts;
+import org.hibernate.Session;
+
+import java.util.List;
 
 public class MyOwnChart extends AbstractHighCharts {
 
-	public JSONObject getComponentOptions(){
-		
-		JSONObject opt = new JSONObject();
-		opt.put("text", "Test Override");
-		opt.put("x", -20);
-		
-		JSONObject high = new JSONObject();
-		high.put("subtitle", opt);
-		high.put("chart", new JSONObject("renderTo", getClientId()));
-		return high;
-		
-	}
+    @Inject
+    private Session session;
+
+
+    public JSONObject getComponentOptions() {
+
+        JSONObject opt = new JSONObject();
+        opt.put("text", "Test Override");
+        opt.put("x", -20);
+
+        JSONObject donneesDuGraphe = new JSONObject();
+        donneesDuGraphe.put("subtitle", opt);
+        donneesDuGraphe.put("chart", new JSONObject("renderTo", getClientId()));
+
+        donneesDuGraphe.put("series", getSeries());
+
+        donneesDuGraphe.put("yAxis", getYAxis());
+        return donneesDuGraphe;
+
+    }
+
+    private JSONObject getYAxis() {
+        JSONObject yaxis = new JSONObject();
+        yaxis.put("title", new JSONObject("text", "Nombre de pièces"));
+        return yaxis;
+    }
+
+    private JSONArray getSeries() {
+        JSONArray series = new JSONArray();
+
+        JSONObject nameData = new JSONObject();
+        JSONArray data = new JSONArray();
+
+        List<Piece> list = session.createCriteria(Piece.class).list();
+        for (Piece piece : list) {
+//            data.put();
+            piece.getArticles().size();
+        }
+        data.put(1.0);
+        data.put(1.0);
+        data.put(1.0);
+        data.put(1.0);
+        data.put(1.0);
+        data.put(1.0);
+        data.put(1.0);
+        data.put(1.0);
+        data.put(1.0);
+        data.put(1.0);
+        data.put(1.0);
+        nameData.put("name", "paris");
+        nameData.put("data", data);
+        series.put(nameData);
+        return series;
+    }
 }
--- src/main/webapp/js/demo.js
+++ src/main/webapp/js/demo.js
@@ -18,9 +18,9 @@
 						'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 				},
 				yAxis: {
-					title: {
+					/*title: {
 						text: 'Temperature (Â°C)'
-					},
+					},*/
 					plotLines: [{
 						value: 0,
 						width: 1,
@@ -40,7 +40,8 @@
 					x: -10,
 					y: 100,
 					borderWidth: 0
-				},
+				}
+				/*,
 				series: [{
 					name: 'Tokyo',
 					data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
@@ -53,7 +54,7 @@
 				}, {
 					name: 'London',
 					data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				}]
+				}]*/
 			});
 		}
 
